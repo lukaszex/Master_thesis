7419bc3556cf51bf44d73de0ad60d5dfbddc54dd
lukaszex <lukaszex1997@gmail.com>
2021-02-09 10:40:09 +0100

    Efficiencyy measures and gitignore added

--- Population.py
+++ Population.py
@@ -7,7 +7,7 @@ import random
 
 class Population:
     def __init__(self, populationID_, generationNumber_, cities_, numberOfSpecimen_, type_, tournamentSize_, p_m_,
-                 eliteSize_, migrationSize_, specimen_, pmx_, cx_, ox_, pmxEff_, cxEff_, oxEff_):
+                 eliteSize_, migrationSize_, specimen_, pmx_, cx_, ox_, pmxEff_, cxEff_, oxEff_, swap_, insert_, scramble_, inversion_, swapEff_, insertEff_, scrambleEff_, inversionEff_):
         self.populationID = populationID_
         self.generationNumber = generationNumber_
         self.cities = cities_
@@ -27,6 +27,14 @@ class Population:
         self.pmxEff = pmxEff_
         self.cxEff = cxEff_
         self.oxEff = oxEff_
+        self.swap = swap_
+        self.insert = insert_
+        self.scramble = scramble_
+        self.inversion = inversion_
+        self.swapEff = swapEff_
+        self.insertEff = insertEff_
+        self.scrambleEff = scrambleEff_
+        self.inversionEff = inversionEff_
         self.parents = pd.DataFrame()
         self.childrenList = []
 
@@ -54,6 +62,10 @@ class Population:
 
     def crossover(self):
         self.specimen = self.parents.iloc[0:self.eliteSize, :]
+        if self.type == 'normal':
+            self.crossoverNormal()
+
+    def crossoverNormal(self):
         for i in range(int(self.parents.shape[0] / 2)):
             parent1 = self.parents.iloc[i, 1]
             parent1Fitness = self.parents.iloc[i, 0]
@@ -66,7 +78,7 @@ class Population:
                 child2 = crossoverPMX(parent2, parent1)
                 child1Fitness = self.evaluateSingleSpeciman(child1)
                 child2Fitness = self.evaluateSingleSpeciman(child2)
-                if np.mean([child1Fitness, child2Fitness]) <= np.mean([parent1Fitness, parent2Fitness]):
+                if min([child1Fitness, child2Fitness]) < min([parent1Fitness, parent2Fitness]):
                     self.pmxEff += 1
             elif crossMethod == 'ox':
                 self.ox += 1
@@ -74,7 +86,7 @@ class Population:
                 child2 = crossoverOX(parent2, parent1)
                 child1Fitness = self.evaluateSingleSpeciman(child1)
                 child2Fitness = self.evaluateSingleSpeciman(child2)
-                if np.mean([child1Fitness, child2Fitness]) <= np.mean([parent1Fitness, parent2Fitness]):
+                if min([child1Fitness, child2Fitness]) < min([parent1Fitness, parent2Fitness]):
                     self.oxEff += 1
             elif crossMethod == 'cx':
                 self.cx += 1
@@ -82,25 +94,45 @@ class Population:
                 child2 = crossoverCX(parent2, parent1)
                 child1Fitness = self.evaluateSingleSpeciman(child1)
                 child2Fitness = self.evaluateSingleSpeciman(child2)
-                if np.mean([child1Fitness, child2Fitness]) <= np.mean([parent1Fitness, parent2Fitness]):
+                if min([child1Fitness, child2Fitness]) < min([parent1Fitness, parent2Fitness]):
                     self.cxEff += 1
             self.childrenList.append(child1)
             self.childrenList.append(child2)
         pass
 
     def mutate(self):
+        if self.type == 'normal':
+            self.mutateNormal()
+
+    def mutateNormal(self):
         for child in self.childrenList:
             if random.random() < self.p_m:
+                beforeFitness = self.evaluateSingleSpeciman(child)
                 mutMethod = random.choice(['swap', 'insert', 'scramble', 'inversion'])
                 if mutMethod == 'swap':
+                    self.swap += 1
                     child = mutationSwap(child)
-                    pass
+                    afterFitness = self.evaluateSingleSpeciman(child)
+                    if afterFitness < beforeFitness:
+                        self.swapEff += 1
                 elif mutMethod == 'insert':
+                    self.insert += 1
                     child = mutationInsert(child)
+                    afterFitness = self.evaluateSingleSpeciman(child)
+                    if afterFitness < beforeFitness:
+                        self.insertEff += 1
                 elif mutMethod == 'scramble':
+                    self.scramble += 1
                     child = mutationScramble(child)
+                    afterFitness = self.evaluateSingleSpeciman(child)
+                    if afterFitness < beforeFitness:
+                        self.scrambleEff += 1
                 elif mutMethod == 'inversion':
+                    self.inversion += 1
                     child = mutationInversion(child)
+                    afterFitness = self.evaluateSingleSpeciman(child)
+                    if afterFitness < beforeFitness:
+                        self.inversionEff += 1
             child = pd.Series([child, np.nan])
             self.specimen = self.specimen.append(child, ignore_index = True)
         self.specimen.iloc[self.eliteSize:, 1] = self.specimen.iloc[self.eliteSize:, 2]
@@ -136,13 +168,15 @@ class Population:
 
 if __name__ == '__main__':
     cities_ = readData('test1')
-    pop = Population(1, 1, cities_, 50, 'normal', 5, 0.05, 10, 4, None, 0, 0, 0, 0, 0, 0)
+    pop = Population(1, 0, cities_, 50, 'normal', 5, 0.05, 10, 4, None, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
     pop.createInitialPopulation()
     pop.evaluate()
-    for it in range(2, 50):
+    for it in range(1, 50):
         pop.selectParents()
         pop.crossover()
         pop.mutate()
         pop.evaluate()
-        pop = Population(1, it, cities_, 50, 'normal', 5, 0.05, 10, 4, pop.specimen, pop.pmx, pop.cx, pop.ox, pop.pmxEff, pop.cxEff, pop.oxEff)
-    print('PMX: {}, OX: {}, CX: {}'.format(pop.pmxEff/pop.pmx, pop.oxEff/pop.ox, pop.cxEff/pop.cx))
+        pop = Population(0, it, cities_, 50, 'normal', 5, 0.05, 10, 4, pop.specimen, pop.pmx, pop.cx, pop.ox, pop.pmxEff, pop.cxEff, pop.oxEff, pop.swap, pop.insert, pop.scramble, pop.inversion,
+                         pop.swapEff, pop.insertEff, pop.scrambleEff, pop.inversionEff)
+    print('PMX: {}, OX: {}, CX: {}, swap: {}, insert: {}, scramble: {}, inversion: {}'.format(pop.pmxEff/pop.pmx,
+          pop.oxEff/pop.ox, pop.cxEff/pop.cx, pop.swapEff/pop.swap, pop.insertEff/pop.insert, pop.scrambleEff/pop.scramble, pop.inversionEff/pop.inversion))
